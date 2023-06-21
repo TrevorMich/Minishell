f1ae0c2379d23a6f5964700eb366dc0d35c46222
Trevor <79858777+TrevorMich@users.noreply.github.com>
2023-06-21 09:02:48 +0100

    added library

---
+++ minishell.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ioduwole <ioduwole@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/21 09:02:03 by ioduwole          #+#    #+#             */
+/*   Updated: 2023/06/21 09:02:17 by ioduwole         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+#include <readline/history.h>
+#include <readline/readline.h>
+#include "libft/libft.h"
+#include <stdio.h>
+
+
+#endif
