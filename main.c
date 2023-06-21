8216335930558c094a9a814afac5c3ed8b2fb445
Trevor <79858777+TrevorMich@users.noreply.github.com>
2023-06-21 08:59:56 +0100

    added main

---
+++ main.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ioduwole <ioduwole@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/21 08:55:36 by ioduwole          #+#    #+#             */
+/*   Updated: 2023/06/21 08:55:55 by ioduwole         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int main(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+	while (1)
+	{
+		char *str = readline("minishell$: ");
+		printf("%s\n", str);
+		free(str);
+	}
+	
+	return (0);
+}
